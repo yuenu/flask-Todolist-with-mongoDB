30ea92fb6aa1046ec80c5521db7605c252fa2c09
yuenu <yuenuni@hotmail.com>
2020-09-02 18:40:39 +0800

    Inital commit

---
+++ todoapp/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from .extensions import mongo
+from .main.routes import main
+
+def create_app():
+    app = Flask(__name__)
+    # mongodb+srv://Admin:<password>@cluster0.yruh1.mongodb.net/<dbname>?retryWrites=true&w=majority
+    app.config['MONGO_URI'] = 'mongodb+srv://Admin:wak6hcjpw2@cluster0.yruh1.mongodb.net/mydb?retryWrites=true&w=majority'
+
+    mongo.init_app(app)
+
+    app.register_blueprint(main)
+
+    return app
